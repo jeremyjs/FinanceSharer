7f633e1b2e93f9ad9e59425a6ca9ec378b39d2a1
somussma <somussma@purdue.edu>
2013-12-03 00:44:38 -0500

    Upload App after Deletion

---
+++ AndroidApp/src/edu/purdue/cs/cs252/channel/MessageListener.java
@@ -0,0 +1,20 @@
+package edu.purdue.cs.cs252.channel;
+
+/**
+ * A listener interface to allow clients and servers to be notified when a new
+ * message is received.
+ * 
+ * @author fmeawad
+ * 
+ */
+public interface MessageListener {
+    /**
+     * Notifies listener of new messages.
+     * 
+     * @param message
+     *            The message received.
+     * @param clientID
+     *            The id of the channel the message was received from.
+     */
+    public void messageReceived(String message, int clientID);
+}
